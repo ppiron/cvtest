c3a9d5d12583d54709f6f639078ded804b43df89
Paolo <ppiron@gmail.com>
2020-10-01 16:49:31 +0100

    Folders restructuring

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
 module.exports = function (eleventyConfig) {
   // eleventyConfig.addFilter( "myFilter", function() {});
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/admin");
 
   return {
     dir: {
-      input: ".",
+      input: "src",
       includes: "_includes",
       layouts: "_layouts",
     },
+    passthroughFileCopy: true,
   };
 };
