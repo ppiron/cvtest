4ce7c6e36c215cc2eca8ef57cf87b262784fdbab
Paolo <ppiron@gmail.com>
2020-10-02 12:45:33 +0100

    Added "makeDate" filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,26 @@
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addFilter( "myFilter", function() {});
+  eleventyConfig.addFilter("makeDate", function (value) {
+    if (typeof value === "string") {
+      return value;
+    }
+    const dat = new Date(Date.parse(value));
+    const months = {
+      0: "January",
+      1: "February",
+      2: "March",
+      3: "April",
+      4: "May",
+      5: "June",
+      6: "July",
+      7: "August",
+      8: "September",
+      9: "October",
+      10: "November",
+      11: "December",
+    };
+    return `${months[dat.getMonth()]}, ${dat.getFullYear()}`;
+  });
+
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
